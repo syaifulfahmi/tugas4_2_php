2d3c58eedb10dc30d1d9e441fd5f06e5940b4bc8
syaifulfahmi <52196122+syaifulfahmi@users.noreply.github.com>
2020-10-07 12:33:18 +0700

    Add files via upload

---
+++ tugas4_2.php
@@ -0,0 +1,9 @@
+<?php
+
+$jml_saluran = 523;
+$terpakai = 8891;
+$rata_rata = $terpakai/$jml_saluran;
+
+echo "Jumlah Saluran = ".$jml_saluran,"<br>","Jumlah Terpakai = ".$terpakai,"<br>","Rata-rata pemakaian = ... ?","<br>","Jawaban : ".$rata_rata," liter/keluarga";
+
+?>
